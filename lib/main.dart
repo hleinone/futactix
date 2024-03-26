fed43109049c1bfc390faf4b31d13b6984009f73
Hannu Leinonen <hleinone@gmail.com>
2024-03-26 22:41:51 +0200

    Added platform menu bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -62,42 +63,203 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: PlatformMenuBar(
+        menus: [
+          PlatformMenu(
+            label: 'Futactix',
+            menus: [
+              PlatformMenuItemGroup(
+                members: [
+                  PlatformMenuItem(
+                    label: 'About Futactix',
+                    onSelected: () {
+                      print('Clicked About Futactix');
+                    },
+                  ),
+                ],
+              ),
+              PlatformMenuItemGroup(
+                members: [
+                  PlatformMenuItem(
+                    label: 'Settings',
+                    onSelected: () {
+                      print('Clicked Settings');
+                    },
+                    shortcut: const SingleActivator(
+                      LogicalKeyboardKey.comma,
+                      meta: true,
+                    ),
+                  ),
+                ],
+              ),
+              if (PlatformProvidedMenuItem.hasMenu(PlatformProvidedMenuItemType.quit))
+                const PlatformProvidedMenuItem(type: PlatformProvidedMenuItemType.quit),
+            ],
+          ),
+          PlatformMenu(
+            label: 'File',
+            menus: [
+              PlatformMenuItemGroup(
+                members: [
+                  PlatformMenuItem(
+                    label: 'New Project',
+                    onSelected: () {
+                      print('Clicked File > New Project');
+                    },
+                    shortcut: const SingleActivator(
+                      LogicalKeyboardKey.keyN,
+                      meta: true,
+                    ),
+                  ),
+                  PlatformMenuItem(
+                    label: 'Open',
+                    onSelected: () {
+                      print('Clicked File > Open');
+                    },
+                    shortcut: const SingleActivator(
+                      LogicalKeyboardKey.keyO,
+                      meta: true,
+                    ),
+                  ),
+                  PlatformMenuItem(
+                    label: 'Save',
+                    onSelected: () {
+                      print('Clicked File > Save');
+                    },
+                    shortcut: const SingleActivator(
+                      LogicalKeyboardKey.keyS,
+                      meta: true,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          PlatformMenu(
+            label: 'Edit',
+            menus: [
+              PlatformMenuItemGroup(
+                members: [
+                  PlatformMenuItem(
+                    label: 'Undo',
+                    onSelected: () {
+                      print('Clicked Edit > Undo');
+                    },
+                    shortcut: const SingleActivator(
+                      LogicalKeyboardKey.keyZ,
+                      meta: true,
+                    ),
+                  ),
+                  PlatformMenuItem(
+                    label: 'Clear',
+                    onSelected: () {
+                      print('Clicked Edit > Clear Drawings');
+                    },
+                  ),
+                  PlatformMenuItem(
+                    label: 'Remove Objects',
+                    onSelected: () {
+                      print('Clicked Edit > Remove Objects');
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          PlatformMenu(
+            label: 'Formations',
+            menus: [
+              PlatformMenuItemGroup(
+                members: [
+                  PlatformMenuItem(
+                    label: 'Football 5',
+                    onSelected: () {
+                      print('Clicked Formations > Football 5');
+                    },
+                  ),
+                  PlatformMenuItem(
+                    label: 'Football 8',
+                    onSelected: () {
+                      print('Clicked Formations > Football 8');
+                    },
+                  ),
+                  PlatformMenuItem(
+                    label: 'Football 11',
+                    onSelected: () {
+                      print('Clicked Formations > Football 11');
+                    },
+                  ),
+                ],
+              ),
+              PlatformMenuItemGroup(
+                members: [
+                  PlatformMenuItem(
+                    label: 'Football 5 vs 5',
+                    onSelected: () {
+                      print('Clicked Formations > Football 5 vs 5');
+                    },
+                  ),
+                  PlatformMenuItem(
+                    label: 'Football 8 vs 8',
+                    onSelected: () {
+                      print('Clicked Formations > Football 8 vs 8');
+                    },
+                  ),
+                  PlatformMenuItem(
+                    label: 'Football 11 vs 11',
+                    onSelected: () {
+                      print('Clicked Formations > Football 11 vs 11');
+                    },
+                  ),
+                ],
+              ),
+              PlatformMenuItemGroup(
+                members: [
+                  PlatformMenuItem(
+                    label: 'Futsal 5',
+                    onSelected: () {
+                      print('Clicked Formations > Futsal 5');
+                    },
+                  ),
+                  PlatformMenuItem(
+                    label: 'Futsal 5 vs 5',
+                    onSelected: () {
+                      print('Clicked Formations > Futsal 5 vs 5');
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
