2a56a91f995acc999c23a77444e41ba79b55b82d
Hannu Leinonen <hleinone@gmail.com>
2024-03-30 22:00:25 +0200

    Added new more icons.

--- lib/src/design/icon.dart
+++ lib/src/design/icon.dart
@@ -37,4 +37,15 @@ class Icons {
   static const IconData poleStandingBackground = IconData._('assets/vector/pole_standing_background.svg');
   static const IconData poleStandingForeground = IconData._('assets/vector/pole_standing_foreground.svg');
   static const IconData marker = IconData._('assets/vector/marker.svg');
+  static const IconData coneBackground = IconData._('assets/vector/cone_background.svg');
+  static const IconData coneForeground = IconData._('assets/vector/cone_foreground.svg');
+  static const IconData fence = IconData._('assets/vector/fence.svg');
+  static const IconData bench = IconData._('assets/vector/bench.svg');
+  static const IconData ladder = IconData._('assets/vector/ladder.svg');
+  static const IconData smallGoal = IconData._('assets/vector/small_goal.svg');
+  static const IconData largeGoal = IconData._('assets/vector/large_goal.svg');
+  static const IconData futsalGoal = IconData._('assets/vector/futsal_goal.svg');
+  static const IconData football11 = IconData._('assets/vector/football_11.svg');
+  static const IconData football11vs11 = IconData._('assets/vector/football_11_vs_11.svg');
+  static const IconData futsal5vs5 = IconData._('assets/vector/futsal_5_vs_5.svg');
 }
